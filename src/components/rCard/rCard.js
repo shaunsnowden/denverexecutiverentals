b308ad1f067c7ed326d2b897fa2f7a5ea2282140
Jacques Guillory <jacquesguillory@Jacquess-MacBook-Pro.local>
2018-01-08 20:19:40 -0700

    added modal popups to residences page

--- src/components/rCard/rCard.js
+++ src/components/rCard/rCard.js
@@ -1,16 +1,52 @@
 import React from "react";
 import "./rCard.css";
+import Modal from 'react-responsive-modal';
+import ReactDOM from 'react-dom';
 
-const RCard = props => (
-    <div className = "rcard">
-        <div className = "img-container" id ={props.id}>
-            <img alt={props.name} src={props.image}  />
+export default class RCard extends React.Component {
+    state = {
+      open: false,
+    };
+  
+    // modal functions
+    onOpenModal = () => {
+      this.setState({ open: true });
+    };
+   
+    onCloseModal = () => {
+      this.setState({ open: false });
+    };
+   
+    render() {
+
+        const { open } = this.state;
+
+        return(
+        
+        // individual card
+        <div className = "rcard">
+            <div className = "img-container" id ={this.props.id}>
+                <img alt={this.props.name} src={this.props.image} onClick={this.onOpenModal}  />
+                
+                {/* animated modal */}
+                <Modal open={open} onClose={this.onCloseModal} little
+                    classNames={{
+                    transitionEnter: 'transition-enter',
+                    transitionEnterActive: 'transition-enter-active',
+                    transitionExit: 'transition-exit-active',
+                    transitionExitActive: 'transition-exit-active',
+                    }}
+                    animationDuration={1000}>
+                <h2>{this.props.name}</h2>
+                <img alt={this.props.name} src={this.props.image} style={{height: "300px", width: "400px"}} />
+                <p>{this.props.description}</p>
+                </Modal>
+            </div>
+            <div className="content">
+                <span>{this.props.name}</span>
+            </div>
         </div>
-        <div className="content">
-            <span>{props.name}</span>
-        </div>
-    </div>
-
-);
-
-export default RCard;
+        );
+        
+    }
+}
