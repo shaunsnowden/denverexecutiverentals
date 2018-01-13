2c8b9b9397c0c6045336564c17763bd551b86b41
unknown <efinley67@gmail.com>
2018-01-13 08:34:39 -0700

    adding callback again

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'snowstorm.auth0.com',
   clientId: '5fyX3MIhTlgLm9a3q4TEQ2Kli4BqW1sj',
-  callbackUrl: 'https://quiet-badlands-88728.herokuapp.com/'
+  callbackUrl: 'https://quiet-badlands-88728.herokuapp.com/callback'
 }
